cb3e1fd6f706efac462c862e7945ab1ab091fda5
Keiko Oda <keiko713@gmail.com>
2013-02-20 00:27:28 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YougoboyApp::Application.config.secret_token = '750ec1391a4a1ddcd68d10b8a0fdb28fffe38f92eab65ff547f8a0e88ad5a048735513cb2e3d51a0a0346975d1e9cd0bb03dccd762e2cbd3e6969a24e5d0dee5'
