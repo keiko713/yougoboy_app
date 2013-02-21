31c4369b77bafd38ee2fb93a1a50d4a99e9b9b54
Keiko Oda <keiko713@gmail.com>
2013-02-21 00:54:08 -0800

    Create seeds for fruits

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# seeds for Fruits
+Word.delete_all
+fruits = [
+  { name: 'Apple', level: 1},
+  { name: 'Banana', level: 1},
+  { name: 'Cherry', level: 1},
+  { name: 'Grape', level: 1},
+  { name: 'Grapefruit', level: 1},
+  { name: 'Lemon', level: 1},
+  { name: 'Melon', level: 1},
+  { name: 'Orange', level: 1},
+  { name: 'Peach', level: 1},
+  { name: 'Pear', level: 2},
+  { name: 'Pineapple', level: 1},
+  { name: 'Strawberry', level: 1},
+  { name: 'Watermelon', level: 2}
+]
+fruits.each do |fruit|
+  Word.create!(name: fruit[:name], level: fruit[:level], group: 'Fruits')
+end
